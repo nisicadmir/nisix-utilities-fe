91b9aa22c45b4a3eccb4d51626e6a9c977141cf0
Admir Nisic <admir.nisic@workpuls.com>
2025-04-22 22:43:57 +0200

    fix loader

--- src/app/_modules/loader/loader.component.ts
+++ src/app/_modules/loader/loader.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, signal } from '@angular/core';
+import { Component, effect, OnInit, signal } from '@angular/core';
 import { LoaderService } from './loader.service';
 
 @Component({
@@ -6,12 +6,12 @@ import { LoaderService } from './loader.service';
   templateUrl: './loader.component.html',
   styleUrls: ['./loader.component.scss'],
 })
-export class LoaderComponent implements OnInit {
+export class LoaderComponent {
   isLoading = signal<boolean>(false);
 
-  constructor(private loaderService: LoaderService) {}
-
-  ngOnInit() {
-    this.isLoading.set(this.loaderService.isLoading());
+  constructor(private loaderService: LoaderService) {
+    effect(() => {
+      this.isLoading.set(this.loaderService.isLoading());
+    });
   }
 }
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
+import { AsyncPipe } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
-import { ThemeService } from './theme.service';
-import { AsyncPipe } from '@angular/common';
 import { LoaderComponent } from './_modules/loader/loader.component';
+import { ThemeService } from './theme.service';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
