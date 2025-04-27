aa478078903d29f5d6548840feb2e9fa91f5dbe7
Admir Nisic <admir.nisic@workpuls.com>
2025-04-27 15:09:56 +0200

    change the way for SEO

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,11 +1,15 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter } from '@angular/router';
+import { provideRouter, withHashLocation } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { basicInterceptor } from './_interceptor/basic.interceptor';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(withInterceptors([basicInterceptor]))],
+  providers: [
+    provideRouter(routes, withHashLocation()),
+    provideAnimations(),
+    provideHttpClient(withInterceptors([basicInterceptor])),
+  ],
 };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,18 +1,17 @@
 import { Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { TimeConverterComponent } from './time-converter/time-converter.component';
-import { PasswordGeneratorComponent } from './password-generator/password-generator.component';
-import { UuidGeneratorComponent } from './uuid-generator/uuid-generator.component';
-import { circuitGameComponent } from './circuit-game/circuit-game.component';
-import { SnakeGameComponent } from './snake-game/snake-game.component';
-import { BattleshipGameComponent } from './battleship-game/battleship-game.component';
-import { GameInviteAcceptComponent } from './game-invite-accept/game-invite-accept.component';
 import { BattleshipGamePlayComponent } from './battleship-game-play/battleship-game-play.component';
-import { TimeConverterSeoComponent } from './time-converter-seo/time-converter-seo.component';
+import { BattleshipGameComponent } from './battleship-game/battleship-game.component';
+import { circuitGameComponent } from './circuit-game/circuit-game.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GameInviteAcceptComponent } from './game-invite-accept/game-invite-accept.component';
+import { PasswordGeneratorComponent } from './password-generator/password-generator.component';
+import { SnakeGameComponent } from './snake-game/snake-game.component';
+import { TimeConverterComponent } from './time-converter/time-converter.component';
+import { UuidGeneratorComponent } from './uuid-generator/uuid-generator.component';
+
 export const routes: Routes = [
   { path: '', component: DashboardComponent },
   { path: 'time-converter', component: TimeConverterComponent },
-  { path: 'time-converter-seo', component: TimeConverterSeoComponent },
   { path: 'password-generator', component: PasswordGeneratorComponent },
   { path: 'uuid-generator', component: UuidGeneratorComponent },
   { path: 'circuit-game', component: circuitGameComponent },
