0fc455b502d62debf57a2f75432cdaa50dce4514
Admir Nisic <ni.admir@gmail.com>
2025-07-21 21:05:09 +0200

    update fe uri

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   url: 'https://www.nisix.net',
-  apiUrl: 'site--nisix-utilities-be--jxxml6l7wt9k.code.run/api/v1',
+  apiUrl: 'https://site--nisix-utilities-be--jxxml6l7wt9k.code.run/api/v1',
 };
