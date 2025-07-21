ded293c7d90347d0fb09cfdaf6e57e98574f5ec8
Admir Nisic <ni.admir@gmail.com>
2025-07-21 20:43:46 +0200

    align BE url

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   url: 'https://www.nisix.net',
-  apiUrl: 'https://nisix-utilities-be.onrender.com/api/v1',
+  apiUrl: 'site--nisix-utilities-be--jxxml6l7wt9k.code.run/api/v1',
 };
