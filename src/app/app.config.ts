d1a6c11feccf1f11b1c16c1c7187bc10ffc3e6a1
Admir Nisic <ni.admir@gmail.com>
2025-10-14 12:52:09 +0200

    routes

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,15 +1,11 @@
 import { ApplicationConfig } from '@angular/core';
 import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideRouter, withHashLocation } from '@angular/router';
+import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { basicInterceptor } from './_interceptor/basic.interceptor';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideRouter(routes, withHashLocation()),
-    provideAnimations(),
-    provideHttpClient(withInterceptors([basicInterceptor])),
-  ],
+  providers: [provideRouter(routes), provideAnimations(), provideHttpClient(withInterceptors([basicInterceptor]))],
 };
